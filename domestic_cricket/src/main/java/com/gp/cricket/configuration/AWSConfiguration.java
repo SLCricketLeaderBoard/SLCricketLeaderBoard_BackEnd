e9ea10a1c2b12ef3a868af1939be13c1e9d1d7bf
Nuwan <nuwanmadhusankarulz@gmail.com>
2020-10-07 16:55:54 +0530

    Update aws config file

--- domestic_cricket/src/main/java/com/gp/cricket/configuration/AWSConfiguration.java
+++ domestic_cricket/src/main/java/com/gp/cricket/configuration/AWSConfiguration.java
@@ -1,5 +1,14 @@
 package com.gp.cricket.configuration;
 
+import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:/aws-config.xml")
+@EnableRdsInstance(databaseName = "club_cricket", 
+dbInstanceIdentifier = "mysqldb", 
+password = "#c1234#1234")
 public class AWSConfiguration {
 
 }
