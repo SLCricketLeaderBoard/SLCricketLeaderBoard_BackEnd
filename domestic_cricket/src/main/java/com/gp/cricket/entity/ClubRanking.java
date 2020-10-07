9d1542d816c11f1a4038fd4f51d7e55d495037b3
Nuwan <nuwanmadhusankarulz@gmail.com>
2020-10-07 18:02:40 +0530

    update packages

--- domestic_cricket/src/main/java/com/gp/cricket/entity/ClubRanking.java
+++ domestic_cricket/src/main/java/com/gp/cricket/entity/ClubRanking.java
@@ -8,8 +8,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
-import com.sun.istack.NotNull;
+
 
 @Entity
 @Table(name = "club_ranking")
