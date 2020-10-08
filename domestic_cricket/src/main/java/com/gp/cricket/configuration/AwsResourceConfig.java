de9ff5921af14c44023e0eef440792970daf7af8
Nuwan <nuwanmadhusankarulz@gmail.com>
2020-10-08 16:52:48 +0530

    Update deploy packages

---
+++ domestic_cricket/src/main/java/com/gp/cricket/configuration/AwsResourceConfig.java
@@ -0,0 +1,14 @@
+package com.gp.cricket.configuration;
+
+import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:/aws-config.xml")
+@EnableRdsInstance(databaseName = "club_cricket", 
+                   dbInstanceIdentifier = "mysqldb", 
+				   password = "123456789")
+public class AwsResourceConfig {
+
+}
